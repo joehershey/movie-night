d2144c931ec05ae4acdf001cdfb78d4939e02898
joehershey <jhershey@wisc.edu>
2021-11-04 18:03:24 -0500

    TMDB connected, can get movie by ID, reflected in GUI (just as a test)

--- app/screens/SearchMoviesScreen.js
+++ app/screens/SearchMoviesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableWithoutFeedback,
   SafeAreaView,
+  TextInput,
 } from "react-native";
 
 import { COLORS, STYLES } from "../assets/saved";
@@ -15,7 +16,32 @@ import TabBar from "../components/TabBar";
 import TopBar from "../components/TopBar";
 
 function SearchMoviesScreen(props) {
-  console.log("on settings");
+  const [movieId, setMovieId] = useState("");
+  const [movie, setMovie] = useState("");
+
+  /* API call endpoint for getMovieByID */
+  const BASEURL = "https://api.themoviedb.org/3/";
+  const APIKEY = "?api_key=3eb4bede8c2782fba9b6b7cd9c56b62c";
+
+  function getMovieByID(tmdb_movie_id) {
+    let TEST = BASEURL + "movie/" + tmdb_movie_id + APIKEY;
+    fetch(TEST, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        setMovie(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <SafeAreaView style={STYLES.container}>
       {/* Top Bar */}
@@ -25,7 +51,35 @@ function SearchMoviesScreen(props) {
         message="Groups"
       ></TopBar>
       {/* Content */}
-      <View style={STYLES.content}></View>
+      <View style={STYLES.content}>
+        <TextInput
+          style={[STYLES.input, STYLES.settingsInput]}
+          onChangeText={setMovieId}
+          textAlign="center"
+          value={movieId}
+          placeholder="Movie id (Try 278)"
+        />
+        <TouchableWithoutFeedback onPress={() => getMovieByID(movieId)}>
+          <View
+            style={[
+              STYLES.btn,
+              {
+                backgroundColor: COLORS.secondary,
+                width: "50%",
+                alignSelf: "center",
+              },
+            ]}
+          >
+            <Text>Get movie {movieId}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <View style={{ alignItems: "flex-start", padding: 10 }}>
+          <Text>Title: {movie.title}</Text>
+        </View>
+        <View style={{ alignItems: "flex-start", padding: 10 }}>
+          <Text>Description: {movie.overview}</Text>
+        </View>
+      </View>
       {/* Tabs */}
       <TabBar navigation={props.navigation} screen="search"></TabBar>
     </SafeAreaView>
