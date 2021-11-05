40ce92102a9d8a04ee4b4be1ec57743fba9ee928
joehershey <jhershey@wisc.edu>
2021-11-05 03:43:59 -0500

    Search screen is basically completely done but should add more movie info in Movie.js

--- app/screens/SearchMoviesScreen.js
+++ app/screens/SearchMoviesScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableWithoutFeedback,
   SafeAreaView,
   TextInput,
+  ScrollView,
 } from "react-native";
 
 import { COLORS, STYLES } from "../assets/saved";
@@ -14,33 +15,11 @@ import { FontAwesome5 } from "@expo/vector-icons";
 
 import TabBar from "../components/TabBar";
 import TopBar from "../components/TopBar";
+import Categories from "../components/Categories";
+import Search from "../components/Search";
 
 function SearchMoviesScreen(props) {
-  const [movieId, setMovieId] = useState("");
-  const [movie, setMovie] = useState("");
-
-  /* API call endpoint for getMovieByID */
-  const BASEURL = "https://api.themoviedb.org/3/";
-  const APIKEY = "?api_key=3eb4bede8c2782fba9b6b7cd9c56b62c";
-
-  function getMovieByID(tmdb_movie_id) {
-    let TEST = BASEURL + "movie/" + tmdb_movie_id + APIKEY;
-    fetch(TEST, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json; charset=utf-8",
-      },
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        setMovie(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+  const [display, setDisplay] = useState("categories");
 
   return (
     <SafeAreaView style={STYLES.container}>
@@ -51,39 +30,93 @@ function SearchMoviesScreen(props) {
         message="Groups"
       ></TopBar>
       {/* Content */}
-      <View style={STYLES.content}>
-        <TextInput
-          style={[STYLES.input, STYLES.settingsInput]}
-          onChangeText={setMovieId}
-          textAlign="center"
-          value={movieId}
-          placeholder="Movie id (Try 278)"
-        />
-        <TouchableWithoutFeedback onPress={() => getMovieByID(movieId)}>
-          <View
-            style={[
-              STYLES.btn,
-              {
-                backgroundColor: COLORS.secondary,
-                width: "50%",
-                alignSelf: "center",
-              },
-            ]}
-          >
-            <Text>Get movie {movieId}</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <View style={{ alignItems: "flex-start", padding: 10 }}>
-          <Text>Title: {movie.title}</Text>
+
+      <View style={[STYLES.content, { backgroundColor: COLORS.primary }]}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 10,
+          }}
+        >
+          <TouchableWithoutFeedback onPress={() => setDisplay("categories")}>
+            <View
+              style={[
+                styles.selectButton,
+                display == "categories" ? styles.selected : styles.unselected,
+                { borderTopLeftRadius: 10, borderBottomLeftRadius: 10 },
+              ]}
+            >
+              <Text
+                style={[
+                  {
+                    color: "white",
+                    fontSize: 20,
+                    alignContent: "center",
+                  },
+                  display == "categories"
+                    ? { fontWeight: "bold" }
+                    : { fontWeight: "normal" },
+                ]}
+              >
+                Categories
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+
+          <TouchableWithoutFeedback onPress={() => setDisplay("search")}>
+            <View
+              style={[
+                styles.selectButton,
+                display == "search" ? styles.selected : styles.unselected,
+                { borderTopRightRadius: 10, borderBottomRightRadius: 10 },
+              ]}
+            >
+              <Text
+                style={[
+                  {
+                    color: "white",
+                    fontSize: 20,
+                    alignContent: "center",
+                  },
+                  display == "search"
+                    ? { fontWeight: "bold" }
+                    : { fontWeight: "normal" },
+                ]}
+              >
+                Search
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
-        <View style={{ alignItems: "flex-start", padding: 10 }}>
-          <Text>Description: {movie.overview}</Text>
-        </View>
+        {display == "categories" && <Categories></Categories>}
+        {display == "search" && <Search></Search>}
       </View>
+
       {/* Tabs */}
       <TabBar navigation={props.navigation} screen="search"></TabBar>
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+    borderRadius: 20,
+  },
+  selected: {
+    backgroundColor: COLORS.secondary,
+  },
+  selectButton: {
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10,
+  },
+  unselected: {
+    backgroundColor: "lightgrey",
+  },
+});
 export default SearchMoviesScreen;
