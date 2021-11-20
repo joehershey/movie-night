711ed34442ea8f3d6ccfca7d032692f45d0da7dd
mtru32 <mark.truttmann32@gmail.com>
2021-11-19 21:55:26 -0600

    Add onPress tests
    Both tests fail due to TypeError: ____ is not a function

--- __tests__/components/ButtonPanel-test.js
+++ __tests__/components/ButtonPanel-test.js
@@ -1,7 +1,12 @@
 import ButtonPanel from "../../app/components/ButtonPanel";
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { TouchableWithoutFeedback } from "react-native";
+import { shallow, configure } from 'enzyme';
+//import nodeModulePackage from 'nodeModulePackage';
 
+configure({adapter: new Adapter()});
 describe('Test Button Panel', () => {
     beforeEach( async () => {
         global.tree = renderer.create(<ButtonPanel />);
@@ -9,5 +14,25 @@ describe('Test Button Panel', () => {
 
     it("Button panel renders properly", async () => {
         await expect(global.tree).toMatchSnapshot();
+    });
+
+    it("Create group button exists", () => {
+        const container = shallow(<ButtonPanel/>);
+        //console.log(container.debug());
+        const createButton = container.find(TouchableWithoutFeedback).first();
+        createButton.props().onPress();
+        expect(createButton.exists());
+    });
+
+    it("Join group button exists", () => {
+        const onp = jest.fn();
+        onp.mockReturnValue('Link on press invoked');
+        const container = shallow(<ButtonPanel onClick={onp}/>);
+        //container.dive().instance().toggleShowCreateGroup = jest.fn();
+        //console.log(container.debug());
+        container.update();
+        const joinButton = container.find(TouchableWithoutFeedback).last();
+        joinButton.props().onPress();
+        expect(joinButton.exists());
     });
 });
