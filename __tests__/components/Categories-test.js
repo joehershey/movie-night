87b42df9b9449561b49aea5e730ed40b1bc5eb7b
mtru32 <mark.truttmann32@gmail.com>
2021-12-05 14:41:59 -0600

    Remove beforeEach

--- __tests__/components/Categories-test.js
+++ __tests__/components/Categories-test.js
@@ -3,11 +3,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 describe('Test Categories', () => {
-    beforeEach( async () => {
-        global.tree = renderer.create(<Categories />);
-    });
 
     it("Categories renders properly", async () => {
-        await expect(global.tree).toMatchSnapshot();
+        const tree = renderer.create(<Categories />);
+        expect(tree).toMatchSnapshot();
     });
 });
