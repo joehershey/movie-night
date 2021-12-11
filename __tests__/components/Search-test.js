e16fdcdfd9985c3a849d5064e6bca276a28a61f1
mtru32 <mark.truttmann32@gmail.com>
2021-12-10 19:44:35 -0600

    Test text input elements in Search

--- __tests__/components/Search-test.js
+++ __tests__/components/Search-test.js
@@ -1,6 +1,11 @@
 import Search from "../../app/components/Search";
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { TouchableWithoutFeedback, TextInput } from "react-native";
+import { shallow, configure } from 'enzyme';
+jest.useFakeTimers(); // Mutes expected API async call error
+configure({adapter: new Adapter()});
 
 describe('Test Search', () => {
     beforeEach( async () => {
@@ -9,5 +14,21 @@ describe('Test Search', () => {
 
     it("Search renders properly", async () => {
         await expect(global.tree).toMatchSnapshot();
+    });
+
+    it("Check for text input and simulate submit press", () => {
+        const container = shallow(<Search />);
+        const onSubmit = container.find(TextInput); // finds onSubmitEditing => getMovies()
+        onSubmit.props().onSubmitEditing(); // calls getMovies()
+        expect(onSubmit.exists());
+    });
+
+    it("Check for on change input and simulate setPage()", () => {
+        loadMore = true
+        const container = shallow(<Search {...loadMore}/>);
+        const onChange = container.find(TextInput); // finds onSubmitEditing => getMovies()
+        console.log(onChange.debug())
+        onChange.props().onChange(); // calls getMovies()
+        expect(onChange.exists());
     });
 });
