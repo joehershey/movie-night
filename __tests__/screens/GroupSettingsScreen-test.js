62e1bbdbfd4f2bdb0ebcc314c11e3baff6810786
mtru32 <mark.truttmann32@gmail.com>
2021-12-12 17:00:10 -0600

    Simulate setting values in GroupSettingScreen

--- __tests__/screens/GroupSettingsScreen-test.js
+++ __tests__/screens/GroupSettingsScreen-test.js
@@ -3,6 +3,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {render, fireEvent} from '@testing-library/react-native';
 
+import { TouchableWithoutFeedback } from "react-native";
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import GroupSettings from '../../app/components/GroupSettings';
+
+
+configure({adapter: new Adapter()});
+
 describe('Test Group Settings Screen', () => {
     beforeEach( async () => {
         let props = {
@@ -17,6 +25,20 @@ describe('Test Group Settings Screen', () => {
         await expect(global.tree).toMatchSnapshot();
     });
 
+    it("Access group settings and simulate setting value functions", () => {
+        props = {
+            isAdmin: true
+        }
+        const container = shallow(<GroupSettingsScreen {...props} />);
+        const groupSettings = container.find(GroupSettings); // finds group settings block
+        console.log(container.debug())
+        //simulate group settings
+        groupSettings.props().setMaxMovies("5");
+        groupSettings.props().setGroupName("test");
+        groupSettings.props().changeGroupSettingsAPI();
+        groupSettings.props().setCode();
+    });
+
     it("Does basic test", () => {
         // const toggleShowPopup = jest.fn()
         const isAdmin = true;
@@ -24,7 +46,6 @@ describe('Test Group Settings Screen', () => {
             isAdmin
         }
         const { getByTestId } = render(<GroupSettingsScreen {...props}/>);
-        console.log(getByTestId)
         fireEvent.press(getByTestId('NewCodeButton'));
 
         //fireEvent.changeText(getByTestId('EditGroupName'), 'JESTGroupName');
