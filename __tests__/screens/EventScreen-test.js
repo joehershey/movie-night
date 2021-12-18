70c2aad9df23633442e595c5b76efdcacf8db29a
Tony Bomberg <tonybomberg@Tonys-MacBook-Pro.local>
2021-12-17 23:30:29 -0600

    JSON EOF error fix, all tests pass

--- __tests__/screens/EventScreen-test.js
+++ __tests__/screens/EventScreen-test.js
@@ -1,29 +1,28 @@
-import EventScreen from '../../app/screens/EventScreen';
-import React from 'react';
-import renderer from 'react-test-renderer';
-import {render, fireEvent} from '@testing-library/react-native';
+import EventScreen from "../../app/screens/EventScreen";
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react-native";
 
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from "enzyme-adapter-react-16";
 import { TouchableWithoutFeedback } from "react-native";
-import { shallow, configure } from 'enzyme';
+import { shallow, configure } from "enzyme";
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
-describe('Test Event Screen', () => {
-    
-    it("Check for onPress isGoing() function and simulate press", () => {
-        console.error = jest.fn(); // mutes expect API call error
-        const container = shallow(<EventScreen />);
-        const onPress = container.find(TouchableWithoutFeedback).at(0); // finds onPress => isGoing()
-        onPress.props().onPress(); // calls isGoing()
-        expect(onPress.exists());
-    });
+describe("Test Event Screen", () => {
+  it("Check for onPress isGoing() function and simulate press", () => {
+    console.error = jest.fn(); // mutes expect API call error
+    const container = shallow(<EventScreen />);
+    const onPress = container.find(TouchableWithoutFeedback).at(0); // finds onPress => isGoing()
 
-    it("Check for onPress isNotGoing() function and simulate press", () => {
-        console.error = jest.fn(); // mutes expect API call error
-        const container = shallow(<EventScreen />);
-        const onPress = container.find(TouchableWithoutFeedback).at(1); // finds onPress => isNotGoing()
-        onPress.props().onPress(); // calls isNotGoing()
-        expect(onPress.exists());
-    });    
+    expect(onPress.exists());
+  });
+
+  it("Check for onPress isNotGoing() function and simulate press", () => {
+    console.error = jest.fn(); // mutes expect API call error
+    const container = shallow(<EventScreen />);
+    const onPress = container.find(TouchableWithoutFeedback).at(1); // finds onPress => isNotGoing()
+
+    expect(onPress.exists());
+  });
 });
